7bf5de08c942adca23c1be7ccd1dcf11e7613eb7
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2020-03-06 16:04:31 -0600

    Combining utils into main script

--- start_docker.sh
+++ start_docker.sh
@@ -37,15 +37,50 @@ General purpose docker launcher with the following features:
   - arbitrary LSF arguments
 EOF
 
-# We can launch in project root dir or ./src.  Test based on existence of utils.sh, and cd to root dir if necessary
-# utils.sh might live in . or ./src, depending on where this script runs 
-if [ -e utils.sh ]; then
-    cd ..
-elif [ ! -e src/utils.sh ]; then 
-    >&2 ERROR: cannot locate src/utils.sh
-    exit 1
-fi
-source src/utils.sh
+function test_exit_status {
+    # Evaluate return value for chain of pipes; see https://stackoverflow.com/questions/90418/exit-shell-script-based-on-process-exit-code
+    rcs=${PIPESTATUS[*]};
+    for rc in ${rcs}; do
+        if [[ $rc != 0 ]]; then
+            NOW=$(date)
+            >&2 echo [ $NOW ] $SCRIPT Fatal ERROR.  Exiting.
+            exit $rc;
+        fi;
+    done
+}
+
+function confirm {
+    FN=$1
+    WARN=$2
+    NOW=$(date)
+    if [ ! -s $FN ]; then
+        if [ -z $WARN ]; then
+            >&2 echo [ $NOW ] ERROR: $FN does not exist or is empty
+            exit 1
+        else
+            >&2 echo [ $NOW ] WARNING: $FN does not exist or is empty.  Continuing
+        fi
+    fi
+}
+
+# Evaluate given command CMD either as dry run or for real with
+#     CMD="tabix -p vcf $OUT"
+#     run_cmd "$CMD"
+function run_cmd {
+    CMD=$1
+    DRYRUN=$2
+
+    NOW=$(date)
+    if [ "$DRYRUN" ]; then
+        >&2 echo [ $NOW ] Dryrun: $CMD
+    else
+        >&2 echo [ $NOW ] Running: $CMD
+        eval $CMD
+        test_exit_status
+        NOW=$(date)
+        >&2 echo [ $NOW ] Completed successfully
+    fi
+}
 
 SCRIPT=$(basename $0)
 LSF_ARGS=""
